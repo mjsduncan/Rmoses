0423d3a06a25de30e5098cd8df1d82b1ec3ab2e7
mjsduncan <mjsduncan@gmail.com>
2013-09-05 19:38:30 -0400

    refined combo parsing, added commentary to GEO data set example, wrote up iris example

--- R/combo.R
+++ R/combo.R
@@ -1,69 +1,71 @@
 # convert logical and arithmetic combo program to R function
+
 library(stringr)
-# quote translated operators in combo string
 
-# ?? ("log", "exp", "floor", "mod"=%%)  operator translation may not be necessary
-mc_ops <- list(c("\\$", "~"), c("and", "&"), c("or", "|"), c("not", "!"))
-
-quote_op <- function(str) {
-  str <- str_replace_all(str, mc_ops[[1]][1], mc_ops[[1]][2])
-  str <- str_replace_all(str, mc_ops[[2]][1], mc_ops[[2]][2])                   # translate logical operators TODO: simplify
-  str <- str_replace_all(str, mc_ops[[3]][1], mc_ops[[3]][2])
-  str <- str_replace_all(str, mc_ops[[4]][1], mc_ops[[4]][2])
-   str <- gsub("(0<\\()", "<\\(0 ", str)                                         # translate 0< operator
-  str <- gsub("([<>=]=)","'\\1'", str)                                          # quote double character operators >=, <=, ==
-  str <- gsub("([-+*/<>!&|])(?=[^=0123456789])", "'\\1'", str, perl = TRUE)  # quote single character operators except in above
-  gsub(" ", ", ", str_trim(str))
-}
-var_list <- function(str) sapply(
-  sapply(str_extract_all(quote_op(str), "~(\\w+)"), unique)
-    , paste, collapse = ", ")
+# translate combo program to R expression
+# TODO: fix error when operators have more than 2 operands
 
 combo2exp <- function(str) {
-  exp <- quote_op(str)
+  mc_ops <- list(c("\\$", "~"), c("and", "&"), c("or", "|"), c("not", "!"), c("mod", "%%"))
+  for(i in seq_along(mc_ops)){
+    str <- str_replace_all(str, mc_ops[[i]][1], mc_ops[[i]][2])                   # translate logical operators
+  }
+# translate 0< operator; quote double character operators >=, <=, ==; mod operator %%;  and single character operators
+  ere_sub <- list(c("(0<\\()", "<\\(0 "), c("([<>=]=)","'\\1'"), c("(%%)","'\\1'"), c("([-+*/<>!&|])(?=[^=0123456789])", "'\\1'"))
+  for(i in seq_along(ere_sub)){
+    str <- gsub(ere_sub[[i]][1], ere_sub[[i]][2], str, perl = TRUE)
+  }
+  gsub(" ", ", ", str_trim(str))
+}
+
+# unfinished:  construct explicit R function from combo program
+var_list <- function(str) sapply(
+  sapply(str_extract_all(combo2exp(str), "~(\\w+)"), unique)
+    , paste, collapse = ", ")
+
+exp2fun <- function(str) {
+  exp <- combo2exp(str)
   paste("function (", var_list(str), ") ", exp, sep = "")
 }
 
 
-# # convert string to nested lists of doubles & triples
-# 
-# combo2exp <- function(str){
-#   paste(                            # add external list function to string
-#   'list(', gsub(                    # replace spaces with commas
-#     "( \\()", " list\\(", gsub(                 # add internal list functions
-#       "[ ]{1,}", ", ",quote_op(str)
-#       )
-#     , perl = TRUE), ')', sep=""
-#   )
-# }
-#
-#  eval(parse(text=combo2exp(str)))  # evaluate string to form list
-#
-# # TODO: convert character vector of combo strings to list of lists and produce vector of results
-#
-# # evaluate atomic list
-# 
-# eval_alist <- function(list){
-#   if(length(list)==2) return(do.call(unlist(head(list, n=1)),tail(list, n=1)))  # evaluate unitary operator
-#   do.call(unlist(head(list, n=1)),tail(list, n=2))                              # evaluate binary operator
-# }
-# 
-# # recursively evaluate nested list
-# 
-# eval_list <- function(list){
-#   if(!is.list(list)) return(list)
-#   if(length(list) > 3) return(print("error: list length > 3"))
-#   if(is.list(unlist(head(list)))) return(print("error: first element is not an operator"))
-#   if(sum(sapply(list, is.list)) == 0) return(eval_alist(list))
-#   list <- lapply(list, eval_list)
-#   eval_list(list)
-# }
-# 
-# # put them together to evaluate combo string
-# 
-# eval_combo <- function(str) eval_list(combo2list(str))
+#### convert string to nested lists of doubles & triples   these functions work for combo syntax in wiki examples
 
-# TODO: construct R function
+combo2list <- function(str){
+  paste(                            # add external list function to string
+  'list(', gsub(                    # replace spaces with commas
+    "( \\()", " list\\(", gsub(                 # add internal list functions
+      "[ ]{1,}", ", ",quote_op(str)
+      )
+    , perl = TRUE), ')', sep=""
+  )
+}
+
+ eval(parse(text=combo2list(str)))  # evaluate string to form list
+
+# TODO: convert character vector of combo strings to list of lists and produce vector of results
+
+# evaluate atomic list
+
+eval_alist <- function(list){
+  if(length(list)==2) return(do.call(unlist(head(list, n=1)),tail(list, n=1)))  # evaluate unitary operator
+  do.call(unlist(head(list, n=1)),tail(list, n=2))                              # evaluate binary operator
+}
+
+# recursively evaluate nested list
+
+eval_list <- function(list){
+  if(!is.list(list)) return(list)
+  if(length(list) > 3) return(print("error: list length > 3"))
+  if(is.list(unlist(head(list)))) return(print("error: first element is not an operator"))
+  if(sum(sapply(list, is.list)) == 0) return(eval_alist(list))
+  list <- lapply(list, eval_list)
+  eval_list(list)
+}
+
+# put them together to evaluate combo string
+
+eval_combo <- function(str) eval_list(combo2list(str))
 
 test1 <- "+ 5 9"                                               # 14
 test2 <- "+ 4 (- (+ 6 3) (- (* 2 2) 1))"                       # 10
--- R/moses.R
+++ R/moses.R
@@ -1,24 +1,9 @@
 #  wrapper/interface functions for moses binary
+
 moses <- function( flags = "", DSVfile = "", output = TRUE) {
-	if(flags == "help") {flags = "--help"; output = ""} else {
-	if(DSVfile != "") flags = paste("--input-file", DSVfile, flags) else {flags = "--version"; output = ""}}
-	system2("moses", args=flags, stdout = output)
+	if(flags == "help") {flags = "--help"; output = ""} else {                                                # moses(help) -> moses --help
+	if(DSVfile != "") flags = paste("--input-file", DSVfile, flags) else {flags = "--version"; output = ""}}  # moses() -> moses --version
+	system2("moses", args=flags, stdout = output)                                                             # run moses on file
 }
 
-# output parsing
-library(stringr)
-
-setwd("~/Desktop/moses sandbox")
-combo.v <- moses("-j2 -W1 -u CLASS","IrisSetosa.txt")
-> str_split_fixed(combo.v, " ", 2)
-     [,1] [,2]                                                    
- [1,] "0"  "not(0<(+(*(-2 exp(*(-1 $PW))) $PW))) "                 
- [2,] "0"  "not(0<(+(*(-1 +(1 $PW) exp(*(-1 $PW))) $PW))) "        
- [3,] "-1" "not(0<(+(*(-1 exp(*(-1 $PW))) $PW))) "                 
- [4,] "-1" "and(not(0<(+(*(-1 exp(*(-1 $PW))) $PW))) 0<($SL)) "    
- [5,] "-1" "and(not(0<(+(*(-1 exp(*(-1 $PW))) $PW))) 0<($SW)) "    
- [6,] "-1" "and(not(0<(+(*(-1 exp(*(-1 $PW))) $PW))) 0<($PL)) "    
- [7,] "-1" "and(not(0<(+(*(-1 exp(*(-1 $PW))) $PW))) 0<($PW)) "    
- [8,] "-1" "or(not(0<(+(*(-1 exp(*(-1 $PW))) $PW))) not(0<($SL))) "
- [9,] "-1" "or(not(0<(+(*(-1 exp(*(-1 $PW))) $PW))) not(0<($SW))) "
-[10,] "-1" "or(not(0<(+(*(-1 exp(*(-1 $PW))) $PW))) not(0<($PL))) "
+# TODO:  guided menu system to set moses flags for data appropriate analysis
